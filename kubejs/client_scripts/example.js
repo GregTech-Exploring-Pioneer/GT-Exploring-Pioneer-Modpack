7b86c54710c33d127e4797639ef8b4b93c83575f
DancingSnow <1121149616@qq.com>
2023-12-09 00:34:47 +0800

    修改推叠，禁用原版工具

--- kubejs/client_scripts/example.js
+++
@@ -1,9 +0,0 @@
-// priority: 0
-
-// Visit the wiki for more info - https://kubejs.com/
-
-console.info('Hello, World! (Loaded client scripts)')
-
-ItemEvents.tooltip(event => {
-    event.add('minecraft:stone', [Text.ofString('测试').color(Color.AQUA)])
-})
