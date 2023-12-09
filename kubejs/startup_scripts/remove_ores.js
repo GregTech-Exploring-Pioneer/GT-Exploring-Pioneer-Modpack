1ad5e69e077e3711ac964a2cbbabda4080c43067
DancingSnow <1121149616@qq.com>
2023-12-09 22:56:59 +0800

    remove ad ores

--- kubejs/startup_scripts/remove_ores.js
+++ kubejs/startup_scripts/remove_ores.js
@@ -1,5 +1,19 @@
 WorldgenEvents.remove(event => {
     event.removeOres(props => {
-        props.blocks = ['occultism:silver_ore', 'occultism:silver_ore_deepslate']
+        props.blocks = [
+            'occultism:silver_ore', 
+            'occultism:silver_ore_deepslate', 
+            'ad_astra:mars_iron_ore', 
+            'ad_astra:mars_diamond_ore', 
+            'ad_astra:moon_iron_ore',
+            'ad_astra:glacio_coal_ore',
+            'ad_astra:glacio_copper_ore',
+            'ad_astra:glacio_iron_ore',
+            'ad_astra:glacio_lapis_ore',
+            'ad_astra:mercury_iron_ore',
+            'ad_astra:venus_coal_ore',
+            'ad_astra:venus_gold_ore',
+            'ad_astra:venus_diamond_ore'
+        ]
     })
 })
